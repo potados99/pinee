58003f8be9700ed9d08b75060b2840cbe277f867
potados99 <potados99@gmail.com>
2022-02-11 05:57:34 +0900

    refactor: 이벤트 처리 로직 간소화!

--- lib/routes/update.ts
+++ lib/routes/update.ts
@@ -1,7 +1,6 @@
 import { Client, Message } from "discord.js";
-import NewPinEventResponder from "../responder/NewPinEventResponder";
-import { contentChanged, isArchived, isByThisBot, isFromDm, isJustPinned, isPinned } from "../utils/message";
 import PinMessageUpdateResponder from "../responder/PinMessageUpdateResponder";
+import { isArchived, isByThisBot, isFromDm, isPinned } from "../utils/message";
 
 export async function onMessageUpdate(client: Client, before: Message, after: Message) {
   if (isByThisBot(client, after)) {
@@ -12,17 +11,8 @@ export async function onMessageUpdate(client: Client, before: Message, after: Me
     return;
   }
 
-  if (!contentChanged(before, after) && isJustPinned(before, after)) {
-    console.log(`New pin event on message '${after.id}'`);
 
-    await new NewPinEventResponder(client, after).handle();
-    return;
-  }
-
-  if (contentChanged(before, after) && (isPinned(after) || await isArchived(client, after))) {
-    console.log(`Update event on pinned-or-archived message '${after.id}'`);
-
-    await new PinMessageUpdateResponder(client, after).handle();
-    return;
+  if (isPinned(after) || await isArchived(client, after)) {
+    await new PinMessageUpdateResponder(after).handle();
   }
 }
