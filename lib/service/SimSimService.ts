48c18e5e9df9da6150ccdf4d77fec6a53d75f447
potados99 <potados99@gmail.com>
2022-02-23 21:58:08 +0900

    fix: 따라할 말이 없을때 처리

--- lib/service/SimSimService.ts
+++ lib/service/SimSimService.ts
@@ -11,8 +11,10 @@ export default class SimSimService {
     if (this.message.content.includes('따라해봐:')) {
       const wordsToRepeat = this.message.content.split('따라해봐:').pop()?.trim();
 
-      if (wordsToRepeat != null) {
+      if (wordsToRepeat != null && wordsToRepeat.length > 0) {
         return wordsToRepeat;
+      } else {
+        return `잘 이해하지 못했어요. 무엇을 따라해야 하는거죠...?`;
       }
     }
 
