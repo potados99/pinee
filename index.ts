d7e058d46f84f916c5e5a59473321aa2d557821a
potados99 <potados99@gmail.com>
2022-08-07 03:05:52 +0900

    chore: 안 쓰는 인자는 제거

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ import {info} from './lib/utils/logging';
 import startBot from './lib/bot';
 import {registerCommands} from './lib/commands';
 
-startBot().then((_) => info('봇을 실행하였습니다.'));
+startBot().then(() => info('봇을 실행하였습니다.'));
 registerCommands().then(() => info('명령을 등록하였습니다.'));
